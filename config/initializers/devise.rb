f3eccccef63ed0ce32a421ce056d172d66934713
edynson <edynson@yaroslab.com>
2016-06-11 23:53:15 -0500

    [refactor] Agregado autenticación con fb parte 1

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,6 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "1606897762958592", "b98275b6f3af54630b8c3c0ac0ce21eb"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
